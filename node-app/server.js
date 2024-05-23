b7d11c1044a80e039445a9ebb0cd65142ef4fe17
Neekzen <neekzen.62@gmail.com>
2024-05-23 14:12:20 +0200

    feat: can now add/delete/update html files

--- node-app/server.js
+++ node-app/server.js
@@ -4,18 +4,18 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 8090;
+const port = process.env.PORT || 3000;
 const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
   try {
-    res.render('index', { title: 'Home', data });
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
   } catch (error) {
     console.log("Error:", error);
     res.status(500).send({
@@ -23,6 +23,28 @@ app.get('/', (req, res) => {
     });
   }
 });
+
+app.get('/test', (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, 'views', 'test.html'));
+  } catch (error) {
+    console.log("Error:", error);
+    res.status(500).send({
+      message: "An error occurred while retrieving data."
+    });
+  }
+});
+
+//app.get('/trois', (req, res) => {
+//  try {
+//    res.sendFile(path.join(__dirname, 'views', 'third.html'));
+//  } catch (error) {
+//    console.log("Error:", error);
+//    res.status(500).send({
+//      message: "An error occurred while retrieving data."
+//    });
+//  }
+//});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
